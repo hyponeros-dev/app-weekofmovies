33be610560fcfc8dfd1c036b28ac97321799c47e
Quentin Collet <q.hyponeros@gmail.com>
2023-10-15 18:09:34 +0200

    tuto #1 completed

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ git push -u origin main
 **Push an existing repository from the command line**
 git remote add origin https://github.com/hyponeros-dev/app-weekofmovies.git
 git push -u origin main
+
+- [x] Trouver un calendrier
+- [x] Traduire en français mois/date
+- [ ] Permettre l'ajout d'un film
+- [ ] Rafraichir à la volée le film du jour
+- [ ] Persister dans le locaStorage
+- [ ] Ajouter un bouton de suppression via event delegation
--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-console.log('coucou');
+// Création d'une nouvelle instance de VanillaCalendar (voir dans fichier js)
+const calendar = new VanillaCalendar({
+  selector: '#myCalendar',
+  onSelect: (data, elem) => {
+    console.log(data);
+  },
+  months: [
+    'Janvier',
+    'Février',
+    'Mars',
+    'Avril',
+    'Mai',
+    'Juin',
+    'Juillet',
+    'Août',
+    'Septembre',
+    'Octobre',
+    'Novembre',
+    'Décembre',
+  ],
+  shortWeekday: ['Di', 'Lu', 'Ma', 'Me', 'Je', 'Ve', 'Sa'],
+  pastDates: false,
+});
